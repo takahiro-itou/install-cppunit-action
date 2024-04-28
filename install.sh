cc800f74b990a64a7d6274de15704a4f78971924
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-28 19:04:48 +0900

    既にインストール済みの場合は何もしない

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 install_prefix=$1
 install_version=$2
 
+if [[ -f "${install_prefix}/lib/libcppunit.a" ]] ; then
+    echo "Already installed : ${install_prefix}"
+    exit  0
+fi
+
 package_name="cppunit-${install_version}"
 package_file="${package_name}.tar.gz"
 
